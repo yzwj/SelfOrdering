39d1de7f63fa3e18921dd9d27dcdf80e3995e669
jason <zhangjunsp@gmail.com>
2019-01-30 13:25:33 +0800

    delte

--- yoda_self_ordering/yoda_self_ordering_cn/yoda_self_ordering_cn/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// yoda_self_ordering_cn.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
